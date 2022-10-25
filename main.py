523b9a9f56f756c8b0c4ecc0c249479cb96efd24
Steven79203 <estevaomendes79203@gmail.com>
2022-10-25 16:40:11 -0300

    Add new models and parameters switching

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
 #!/usr/bin/python
 
-# Projeto Trainee CodeQ 2022/2
-
 # Programa em interface gráfica como 
 # demonstração da aplicação de modelos
 # matemáticos na resolução de 
 # problemas comuns à engenharia
 
-"""
-    TODO:
-        > GUI
-        > Mostrar o plot dos modelos
-        > Mostrar a expressão - OK
-        > Espaços para inserção dos parâmetros no modelo
-"""
-
-# 1 - Massa de sal em um tanque
 from matplotlib import gridspec
 from scipy.integrate import odeint 
 from scipy.integrate import quad
@@ -30,111 +19,53 @@ from tkinter import *
 
 sym.init_printing(use_unicode=True)
 
-def plot(data):
-    figure = plt.Figure(figsize=(5,4), dpi=80,)
-    figure.add_subplot(111, xlabel="time(min)", ylabel="mass (g)").plot(data[0],data[1])
-    chart = FigureCanvasTkAgg(figure, result_label)
-    chart.draw()
-    chart.get_tk_widget().place(x=-1,y=-1,width=480,height=480)
 
-def tank():
-    
-    Fin, Fout, Cin, V = 3, 3, 2, 300
-    def dQdt(Q,t, Fin, Fout, Cin):
-        return Fin*Cin - Fout*(Q/V)
-    
-    Fin  = float(input("Vazão de entrada (L/min): "))
-    Fout = float(input("Vazão de saída (L/min): "))
-    Cin  = float(input("Concentração na alimentação (g/L): "))
-    V    = float(input("Volume do tanque (L): "))
-    tf   = float(input("Tempo final de integração (min): "))
-
-    ti = 0
-    Q0 = float(input('Quantidade inicial de sal no tanque(g):'))
-
-    t = np.arange(ti,tf,1)
-
-    sol = odeint(dQdt, Q0, t, args=(Fin, Fout, Cin))
-
-    plt.plot(t, sol, label='massa de sal')
-    plt.legend()
-    plt.grid()
-    plt.xlabel('t (min)')
-    plt.ylabel('m (g)')
-    plt.show()
-
-def tank_function():
-    def dQdt(Q,t, Fin, Fout, Cin):
-        return Fin*Cin - Fout*(Q/V)
-    
-    Fin  = float(Fin_entry.get())
-    Fout = float(Fout_entry.get())
-    Cin  = float(Cin_entry.get())
-    V    = float(Vol_entry.get())
-    Q0   = float(Q0_entry.get())
-    tf   = int(ftime_entry.get())
-
-    t = np.arange(0,tf,1)
-
-    sol = odeint(dQdt, Q0, t, args=(Fin, Fout, Cin))
-    return (t, sol)
-
-# 2.1 - Reator batelada - Reação de 1ª Ordem
-def batch_reactor():
-    def first(X, k):
-        return 1/(k*(1-X))
-
-    k = 0.1
-    V = quad(first, 0, 0.8, args=(k))
-    print(V)
-
-# 2.2 - Reatores contínuos
-def cont_reactors(order="first"):
-    def first(X, v, k, Cao):
-      return (v/k*Cao) * (1/(1-X))
-
-    def second(X, v, k, Cao):
-      return 
-
-    def third(X, v, k, Cao):
-      pass
-
-    k   = 1  # Velocidade específica da reação
-    Cao = 1  # Concentração inicial
-    v   = 1   # Vazão volumétrica
-
-    if(order == "first"):
-        V = quad(first, 0, 0.8, args=(v, k, Cao))
-        return
-    if(order == "second"):
-        V = quad(second, 0, 0.8, args=(k, Cao))
-        return
-    if(order == "third"):
-        V = quad(third, 0, 0.8, args=(k, Cao))
-        return
-
-
-# Symbolic Formulas
+# Symbolic expressions and solution
 def sym_tank():
-    Fin = float(Fin_entry.get())
-    Fout = float(Fout_entry.get())
-    Cin = float(Cin_entry.get())
-    V = float(Vol_entry.get())
+
+    def tank(Q, t, Fin, Fout, Cin, V):
+        return Fin*Cin - Fout*(Q/V)
+
+    Fin   = float(Fin_entry.get())
+    Fout  = float(Fout_entry.get())
+    Cin   = float(Cin_entry.get())
+    V     = float(Vol_entry.get())
+    ftime = float(ftime_entry.get())
+    Q0    = float(Q0_entry.get())
 
     t = sym.symbols('t')
     Q = sym.Function('Q')(t)
     dQdt = Q.diff(t)
     expr = sym.Eq(Fin*Cin - Fout*(Q/V), dQdt)
 
-    F = sym.dsolve(expr, ics={Q.subs(t,0):0})
-    return F
+    t_range = np.arange(0, ftime, 1)
+    F = sym.dsolve(expr, ics={Q.subs(t,Q0):0})
+#    y = [F.subs(t,time).rhs for time in t_range]
+    y = odeint(tank, Q0, t_range, args=(Fin, Fout, Cin, V))
+
+    return (F, t_range, y)
 
 def sym_reactor_batch():
     X = sym.symbols('X')
+    
+    order = int(order_var.get())
+    k     = float(k_entry.get())
+    Cao   = float(Cao_entry.get())
+    V     = float(batch_vol_entry.get())
 
-    k = 0.01
-    exp = 1/(-k*(1-X))
-    return sym.integrate(exp,X)
+    if(order == 1):
+        exp = 1/(k*(1-X))
+    elif(order == 2):
+        exp = 1/(k*Cao) * (1/(1-X)**2)
+    else:
+        exp = 1/(k*(1-X))
+
+    F = sym.integrate(exp,X)
+
+    X_range = np.arange(0,1,0.01)
+    t_range = [sym.re(exp.subs(X,x)) for x in X_range]
+    
+    return (F, X_range, t_range)
 
 def sym_reactor_pfr():
     X = sym.symbols("X")
@@ -145,38 +76,120 @@ def sym_reactor_pfr():
     return sym.integrate(exp,X)
 
 def sym_circuits():
-    pass
+
+    def circuits(I, t, E, R, L):
+        return (E-(R*I))/L
+
+    E  = float(tension_entry.get())
+    R  = float(resist_entry.get())
+    I0 = float(init_curr_entry.get())
+    L  = float(induction_entry.get())
+    tf = float(time_range_entry.get())
+
+    t = sym.symbols('t')
+    I = sym.Function('I')(t)
+    dIdt = sym.diff(I,t)
+    exp  = sym.Eq((E-(R*I))/L, dIdt)
+    F = sym.dsolve(exp, ics={I.subs(t,0):I0})
+    
+    t_range = np.arange(0,tf,0.01)
+    sol = odeint(circuits, I0, t_range, args=(E,R,L))
+    #y = [F.subs(t,x).evalf().rhs for x in t_range]
+    
+    return (F, t_range, sol)
+
+def sym_circuits_2():
+
+    def circuits_2(I,t):
+        return 2*np.sin(t) - 20*I
+
+    t = sym.symbols('t')
+    I = sym.Function('I')(t)
+    I0 = float(init_curr_entry.get())
+    tf = float(time_range_entry.get())
+    
+    dIdt = sym.diff(I,t)
+    exp = sym.Eq(2*sym.sin(t) - 20*I, dIdt)
+    F = sym.dsolve(exp, ics={I.subs(t,0):I0})
+
+    t_range = np.arange(0, tf, 0.01)
+    sol = odeint(circuits_2, I0, t_range)
+#   y = [F.subs(t,x).rhs for x in t_range]
+
+    return (F, t_range, sol)
 
 
-def set_formula(function):
-    exp_label=Label(app, text=function(), background=formula_bg, foreground="#FFFFFF")
-    exp_label.place(x=wpos["exp"][0], y=wpos["exp"][1], width=wpos["exp"][2], height=wpos["exp"][3])
-
-def set_result(function):
-    result_label = Label(app, text="Result = {:2.2f} gramas de sal".format(function()), background=result_bg, foreground="#FFF")
-    result_label.place(x=wpos["res"][0],y=wpos["res"][1], width=wpos["res"][2], height=wpos["res"][3])
-    ## TODO : Determine uma nova string posfixa para cada resultado
+# Clear frame
+def clearframe(frame):
+    for widget in frame.winfo_children():
+        widget.destroy()
 
 
-def get_data():
-    model = model_entry.get()
+# Set data for the widgets
+def set_data():
+    def set_formula(exp):
+        exp_label=Label(app, text=exp, background=formula_bg, foreground="#FFFFFF")
+        exp_label.place(x=wpos["exp"][0], y=wpos["exp"][1], width=wpos["exp"][2], height=wpos["exp"][3])
+
+    def plot(data, x_label, y_label):
+        clearframe(result_label)
+        figure = plt.Figure(figsize=(5,4), dpi=80,)
+        figure.add_subplot(111, xlabel=x_label, ylabel=y_label).plot(data[0],data[1])
+        chart = FigureCanvasTkAgg(figure, result_label)
+        chart.draw()
+        chart.get_tk_widget().pack(fill="both", expand=True)
+
+    model = model_var.get()
     if(model == "tank"):
-        set_formula(sym_tank)
-        #set_result(tank_function, "gramas de sal")
-        data = tank_function()
-        # TODO: Também delimite uma área de plot
-        plot(data)
+        data = sym_tank()
+        set_formula(data[0])
+        plot(data[1::],"time (min)", "mass (g)")
     if(model == "pfr"):
-        set_formula(sym_reactor_pfr)
+        data = sym_reactor_pfr()
+        set_formula(data[0])
+        plot(data[1::], "conversão(X)", "Volume (dm³)")
     if(model == "batch"):
-        set_formula(sym_reactor_batch)
-    if(model == "circuit"):
-        set_formula(sym_circuits)
+        data = sym_reactor_batch()
+        set_formula(data[0])
+        plot((data[1::]), "conversão(X)", "tempo(min)")
+    if(model == "circuits"):
+        data = sym_circuits()
+        set_formula(data[0])
+        plot((data[1::]), "tempo(min)","corrente(A)")
+    if(model == "circuits_2"):
+        data = sym_circuits_2()
+        set_formula(data[0])
+        plot((data[1::]), "tempo(min)","corrente(A)")
+# Set model to use
+def set_model():
+    model = model_var.get()
+    clearframe(params)
+    draw_model_switcher()
+    if(model == "batch" or model == "pfr"):
+        draw_reactor_parameters()
+        return
+    if(model == "tank"):
+        draw_tank_widgets() 
+        return
+    if(model == "circuits" or model == "circuits_2"):
+        draw_circuits_parameters()
+        return
 
+
+# Widgets
+def draw_header(header_title):
+    global header
+    header = Label(app, 
+                text=header_title, 
+                borderwidth=2, relief="groove",
+                font=("Arial Black",20), 
+                background="#570091", foreground="#FFFFFF")
+    header.place(x=wpos["header"][0], y=wpos["header"][1], width=wpos["header"][2], height=wpos["header"][3])
 
 def draw_tank_widgets():
+    draw_header("Massa de sal em tanque")
 # Fin
-    global Fin_entry, Fout_entry, Cin_entry, Vol_entry, Q0_entry, ftime_entry
+    global Fin_entry, Fout_entry, Cin_entry, Vol_entry, Q0_entry, ftime_entry 
     Fin_label  = Label(params, text="Vazão de entrada(L/min)", background=labels_bg, foreground="#FFF", anchor="w")
     Fin_label.place(x=10,y=85, width=290, height=20)
     Fin_entry = Entry(params)
@@ -212,99 +225,164 @@ def draw_tank_widgets():
     ftime_entry  = Entry(params)
     ftime_entry.place(x=10,y=425, width=290, height=20)
 
-def draw_reactor_widgets():
-    pass
+def draw_reactor_parameters():
+    draw_header("Tempo de reação no reator batelada")
+    global Cao_entry, k_entry, batch_vol_entry, flux_entry, order_var
+    
+    Cao_label = Label(params, text="Concentração inicial", background=labels_bg, foreground="#FFFFFF", anchor="w")
+    Cao_label.place(x=10,y=85,width=290, height=20)
+    Cao_entry = Entry(params)
+    Cao_entry.place(x=10,y=115, width=290, height=20)
 
-def draw_circuits_widgets():
-    pass
+    k_label = Label(params, text="Velocidade específica da reação", background=labels_bg, foreground="#FFFFFF", anchor="w")
+    k_label.place(x=10, y=145, width=290, height=20)
+    k_entry = Entry(params)
+    k_entry.place(x=10, y=175, width=290, height=20)
 
+    batch_vol_label = Label(params, text="Volume do batelada", background=labels_bg, foreground="#FFFFFF", anchor="w")
+    batch_vol_label.place(x=10, y=205, width=290, height=20)
+    batch_vol_entry = Entry(params)
+    batch_vol_entry.place(x=10, y=235, width=290, height=20)
+
+    flux_label = Label(params, text="Vazão volumétrica", background=labels_bg, foreground="#FFFFFF", anchor="w")
+    flux_label.place(x=10, y=265, width=290, height=20)
+    flux_entry = Entry(params)
+    flux_entry.place(x=10,y=295, width=290, height=20)
+
+    order_var = StringVar()
+    order_label = Label(params, text="Ordem da reação", background=labels_bg, foreground="#FFFFFF", anchor="w")
+    order_label.place(x=10,y=325, width=290, height=20)
+    
+    order_radio_1 = Radiobutton(params, text="1ª", value=1, variable=order_var)
+    order_radio_2 = Radiobutton(params, text="2ª", value=2, variable=order_var)
+    order_radio_3 = Radiobutton(params, text="3ª", value=3, variable=order_var)
+
+    order_radio_1.place(x=10, y=365, width=60, height=20)
+    order_radio_2.place(x=80, y=365, width=60, height=20)
+    order_radio_3.place(x=150, y=365, width=60, height=20)
+
+def draw_circuits_parameters():
+    draw_header("Corrente em um circuito")
+    global tension_entry, resist_entry, init_curr_entry, induction_entry, time_range_entry
+    tension_label = Label(params, text="Tensão inicial", background=labels_bg, foreground="#FFFFFF",anchor="w")
+    tension_label.place(x=10,y=85,width=290, height=20)
+    tension_entry = Entry(params)
+    tension_entry.place(x=10, y=115, width=290, height=20)
+
+    resist_label = Label(params, text="Resistência(ohms)", background=labels_bg, foreground="#FFFFFF", anchor="w")
+    resist_label.place(x=10,y=145, width=290, height=20)
+    resist_entry = Entry(params)
+    resist_entry.place(x=10, y=175, width=290, height=20)
+
+    init_curr_label = Label(params, text="Corrente inicial (A)", background=labels_bg, foreground="#FFFFFF", anchor="w")
+    init_curr_label.place(x=10,y=205, width=290, height=20)
+    init_curr_entry = Entry(params)
+    init_curr_entry.place(x=10, y=235, width=290, height=20)
+
+    induction_label = Label(params, text="Indutância(H)", background=labels_bg, foreground="#FFFFFF", anchor='w')
+    induction_label.place(x=10, y=265, width=290, height=20)
+    induction_entry = Entry(params)
+    induction_entry.place(x=10, y=295, width=290, height=20)
+
+    time_range_label = Label(params, text="Intervalo de integração (min)", background=labels_bg, foreground="#FFFFFF", anchor='w')
+    time_range_label.place(x=10, y=325, width=290, height=20)
+    time_range_entry = Entry(params)
+    time_range_entry.place(x=10, y=355, width=290, height=20)
+
+
+# Windows
 def aboutMe():
     about = Tk()
     about.title("Sobre")
     app.geometry("300x250")
-    title = Label(about, text="Programa Trainee CodeQ 2022/2",font=("Arial Bold",12))
+    title = Label(about, text="ModelDemo",font=("Arial Bold",12))
     title.place(x=10, y=10, width=280, height=50)
     content = Label(about, text="Programa desenvolvido para o processo\n trainee da CodeQ em 2022/2")
     content.place(x=10,y=70, width=280, height=50)
-    autors = Label(about, text="Autores:\nEstêvão Mendes\nYuri Mewius\nVítor")
+    autors = Label(about, text="Autores:\nEstêvão Mendes Santana\nYuri Mewius\nVítor Soares Gonçalves")
     autors.place(x=10, y=130, width=280, height=90)
     about.mainloop()
 
-app = Tk()
-app.title("CodeQ Trainee Program")
-app.geometry("1000x700")
+def main_app():
+    global app
+    app = Tk()
+    app.title("Modelagem computacional")
+    app.geometry("1000x700")
+    
+    menuBar = Menu(app)
+    
+    menuFile = Menu(menuBar, tearoff=0)
+    menuFile.add_command(label="Fechar", command=app.quit)
+    menuBar.add_cascade(label="Arquivos", menu=menuFile)
+    
+    menuAbout = Menu(menuBar, tearoff=0)
+    menuAbout.add_command(label="Ajuda")
+    menuAbout.add_command(label="Sobre", command=aboutMe)
+    menuBar.add_cascade(label="Sobre", menu=menuAbout)
+    
+    app.config(menu=menuBar)
+    app.configure(background="#B9F")
 
-menuBar = Menu(app)
+def draw_model_switcher():
+    # Model switch
+    global model_var, model, model_entry
+    model_var = StringVar()
+    models = ["tank", "batch", "pfr", "circuits", "circuits_2"]
+    
+    model  = Label(params, text="Modelo",background=labels_bg, foreground="#FFF", anchor="w")
+    model.place(x=10, y=40, width=50, height=20)
+    
+    model_entry  = OptionMenu(params, model_var, *models)
+    model_entry.place(x=60, y=40, width=75, height=30)
 
-menuFile = Menu(menuBar, tearoff=0)
-menuFile.add_command(label="Fechar", command=app.quit)
-menuBar.add_cascade(label="Arquivos", menu=menuFile)
+    model_set = Button(params, text="Set Model", command=set_model)
+    model_set.place(x=145, y=40, height=30)
 
-menuAbout = Menu(menuBar, tearoff=0)
-menuAbout.add_command(label="Ajuda")
-menuAbout.add_command(label="Sobre", command=aboutMe)
-menuBar.add_cascade(label="Sobre", menu=menuAbout)
 
-app.config(menu=menuBar)
-app.configure(background="#B9F")
+# Main Widgets
+def draw_main_widgets():
+    global exp_label, result_label, params
+   
+    # Formula label
+    exp_label=Label(app, text="Fórmula", background="#570091", foreground="#FFFFFF")
+    exp_label.place(x=wpos["exp"][0], y=wpos["exp"][1], width=wpos["exp"][2], height=wpos["exp"][3])
+    
+    # Result label
+    result_label=Label(app, text="Plot", background="#9a34cc", foreground="#FFF")
+    result_label.place(x=wpos["res"][0], y=wpos["res"][1], width=wpos["res"][2], height=wpos["res"][3])
+    #plot((0,0))
+    
+    # Parameters label
+    params=Label(app, text="Parameters", background=labels_bg, foreground="#FFF", anchor="n")
+    params.place(x=wpos["params"][0], y=wpos["params"][1], width=wpos["params"][2], height=wpos["params"][3])
+    
+    Button(app,text="Calcular",command=set_data).place(
+    x=wpos["button"][0], y=wpos["button"][1], width=wpos["button"][2], height=wpos["button"][3])
 
-# Common variables
+
+# Widgets colors
 header_bg  = ""
 labels_bg  = "#6400cd"
 formula_bg = "#570091"
 result_bg  = "#9A34CC"
 
+# Widgets measurements
 wpos = {
-   "header": [10,  10, 810,  50],
-   "exp"   : [10,  70, 480,  50],
+   "header": [10,  10,  810, 50 ],
+   "exp"   : [10,  70,  480, 50 ],
    "res"   : [10,  130, 480, 470],
-   "params": [510, 70, 310, 470],
-   "button": [510, 550,310, 40]
+   "params": [510, 70,  310, 470],
+   "button": [510, 550, 310, 40 ]
 }
 
-# Cabeçalho
-header = Label(app, 
-            text="Programa Trainee CodeQ 2022/2", 
-            borderwidth=2, relief="groove",
-            font=("Arial Black",20), 
-            background="#570091", foreground="#FFFFFF")
-header.place(x=wpos["header"][0], y=wpos["header"][1], width=wpos["header"][2], height=wpos["header"][3])
+# Tank entries positions
+tpos = {
 
-# Formula label
-exp_label=Label(app, text="Fórmula", background="#570091", foreground="#FFFFFF")
-exp_label.place(x=wpos["exp"][0], y=wpos["exp"][1], width=wpos["exp"][2], height=wpos["exp"][3])
-#exp_label.pack(ipadx=10, ipady=10, padx=10, pady=10, side='top', expand=True)
+}
 
-# Result label
-result_label=Label(app, text="Resultado:", background="#9a34cc", foreground="#FFF")
-result_label.place(x=wpos["res"][0], y=wpos["res"][1], width=wpos["res"][2], height=wpos["res"][3])
-plot((0,0))
-#result_label.pack(ipadx=10, ipady=10, padx=10, pady=10, side="left")
+main_app()
+draw_main_widgets()
+draw_model_switcher()
+draw_circuits_parameters()
 
-# Parameters label
-params=Label(app, text="Parameters", background=labels_bg, foreground="#FFF", anchor="n")
-params.place(x=wpos["params"][0], y=wpos["params"][1], width=wpos["params"][2], height=wpos["params"][3])
-#txt3.pack(ipadx=10, ipady=10, padx=10, pady=10, side="right")
-
-# Model switch
-model  = Label(params, text="Modelo",background=labels_bg, foreground="#FFF", anchor="w")
-model.place(x=10, y=25, width=290, height=20)
-model_entry = Entry(params)
-model_entry.place(x=10, y=55, width=290, height=20)
-
-# TODO: Implement buttons to set each model to use
-draw_tank_widgets()
-Button(app,text="Calcular",command=get_data).place(
-    x=wpos["button"][0], y=wpos["button"][1], width=wpos["button"][2], height=wpos["button"][3])
-
-# Ao clicar botão
-# Verificar qual evento chamar
-# Se tanque ou reator ou circuitos
-# Chame as devidas funções com os devidos parâmetros
-# Retorne o resultado para os devidos labels/txts
-
-# TODO:
-    # Create plot region
-    # Create parameter controls 
-    # Expression visualization
 app.mainloop()
